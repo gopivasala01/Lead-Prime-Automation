6c0f643bc7adb1d530aeec642c1e9678e6c77c73
gopivasala01 <49265100+gopivasala01@users.noreply.github.com>
2021-12-07 16:48:03 +0530

    Add files via upload

---
+++ AppConfig.java
@@ -0,0 +1,22 @@
+package LeadPrime_Automation;
+
+public class AppConfig 
+{
+	public static String browserType = "webdriver.chrome.driver";
+	public static String browserPath = "D:\\Softwares\\Selenium files3\\Chrome Drivers\\chromedriver_96.exe"; 
+	public static String userCredentialsFilePath = "D:\\Projects\\LeadPrime Automation\\IDMART Names & Pwds (5).xlsx";
+	public static String indiaMartURL = "https://my.indiamart.com/";
+    public static String userName = "7097895020";
+    public static String password = "9669970040";
+    public static String productType = "Cement";
+    public static String filePath = "D:\\Projects\\LeadPrime Automation\\IndiaMart - Copy.xlsx";
+    public static String leadPrimeCompanyID = "Bdmart";
+    public static String leadPrimeUserNAme = "sanjay";
+    public static String leadPrimePassword = "BM@rt123@sanjay";
+    public static String[] usersInLeadPrime = {"Sanjay H", "Supriya G"}; 
+    public static String leadPrimeURL = "http://weblogin.leadprime.com/";
+    public static String assignedUserForOutOfHYD = "Out of Station";
+    public static String areaLocationsSheet = "D:\\Projects\\LeadPrime Automation\\Area Locations.xlsx";
+    public static String LeadPrimeStatus = "Yes";// No denotes that Both IndiaMart and LeadPrime applications run
+
+}
